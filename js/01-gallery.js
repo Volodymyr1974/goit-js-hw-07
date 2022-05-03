a388471e191ab78e80c3fdee61399cf1bf50fc8e
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-05-04 00:54:06 +0300

    додає рішення ДЗ№1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,61 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgDivContainerEl = document.querySelector(".gallery");
+
+function makeImgCardMarkup({ preview, original,description} ) {           
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`};
+  
+function makeImgGalleryMarkup(Items) {
+    return Items
+    .map(makeImgCardMarkup)
+    .join('');
+}
+const markup = makeImgGalleryMarkup(galleryItems);
+
+
+
+imgDivContainerEl.insertAdjacentHTML('beforeend', markup);
+imgDivContainerEl.addEventListener('click', onImgDivContainer);
+
+function onImgDivContainer(event) {
+    event.preventDefault()
+    const isGalleryImageEl = event.target.classList.contains('gallery__image');
+    if (!isGalleryImageEl) {
+        return;
+    }
+    console.log(event.target);
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`)
+
+    instance.show();
+
+    document.addEventListener("keydown", (event) =>{
+     if (event.code === "Escape") {
+    instance.close();
+        }
+});
+};
+
+
+
+
+
+
+
+
+
+
